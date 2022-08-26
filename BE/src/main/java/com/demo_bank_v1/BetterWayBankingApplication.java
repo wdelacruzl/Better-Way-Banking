7ed9ed9d7964bf08c3281c9b012d9f9bafd9e966
wdelacruzl <wdelacruzl98@gmail.com>
2022-08-26 10:18:50 -0400

    firts commit

---
+++ BE/src/main/java/com/demo_bank_v1/BetterWayBankingApplication.java
@@ -0,0 +1,13 @@
+package com.demo_bank_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterWayBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterWayBankingApplication.class, args);
+	}
+
+}
