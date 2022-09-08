f5cbc25fe63168df42a6d6c483e0d1ddac2874b0
wdelacruzl <wdelacruzl98@gmail.com>
2022-09-08 13:52:35 -0400

    adding readme plus fixing bugs

--- BE/src/main/java/com/demo_bank_v1/BetterWayBankingApplication.java
+++ BE/src/main/java/com/demo_bank_v1/BetterWayBankingApplication.java
@@ -2,10 +2,17 @@ package com.demo_bank_v1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BetterWayBankingApplication{
+public class BetterWayBankingApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
+		return applicationBuilder.sources(BetterWayBankingApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterWayBankingApplication.class, args);
